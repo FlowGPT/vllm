7ff6d24816670e892c449678bcba990de53c7078
KevinCheung2259 <2651309292@qq.com>
2025-07-25 02:59:08 +0000

    [Feat] Support min_p in spec dec

--- vllm/v1/spec_decode/utils.py
+++ vllm/v1/spec_decode/utils.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 from vllm.v1.worker.gpu_input_batch import InputBatch
+import os
 
 
 def is_spec_decode_supported(req_id: str, input_batch: InputBatch) -> bool:
     if req_id in input_batch.min_p_reqs:
         # Spec decode doesn't support min_p sampling.
-        return False
+        return os.environ.get("VLLM_SPEC_DECODE_MIN_P_SUPPORTED", "false").lower() == "true"
     elif (req_id in input_batch.frequency_penalties_reqs
           or req_id in input_batch.presence_penalties_reqs
           or req_id in input_batch.repetition_penalties_reqs):
